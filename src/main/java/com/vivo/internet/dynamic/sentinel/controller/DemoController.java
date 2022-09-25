b313d5d65dc81b032948db8b4bec6827351acf2d
“sd1875819” <sd_sunjin@163.com>
2022-09-25 19:34:57 +0800

    添加一些工程分析

--- src/main/java/com/vivo/internet/dynamic/sentinel/controller/DemoController.java
+++ src/main/java/com/vivo/internet/dynamic/sentinel/controller/DemoController.java
@@ -18,11 +18,13 @@ public class DemoController {
     @Resource
     private TestSentinelService testSentinelService;
 
+    //启动spring工程后，通过在浏览器访问localhost:8080/demo/test 在浏览器输出ok，来确认该工程是否启动成功
     @GetMapping("/test")
     public String test() {
         return "ok";
     }
 
+    //启动spring工程后，通过在浏览器访问localhost:8080/demo/test2 可调用该test2()方法，从而触发里面的testSentinelService.testMehod();逻辑
     @GetMapping("/test2")
     public String test2() {
         testSentinelService.testMehod();
