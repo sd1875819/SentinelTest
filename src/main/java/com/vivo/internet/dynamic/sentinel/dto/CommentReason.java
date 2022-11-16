c079b4b81ba582fd95fcef2934ae87937f037b08
sd_vivo2020 <sd_vivo2020@bbktel.com>
2022-11-16 18:20:35 +0800

    读取本地excel文件并解析成指定格式的json数据

---
+++ src/main/java/com/vivo/internet/dynamic/sentinel/dto/CommentReason.java
@@ -0,0 +1,9 @@
+package com.vivo.internet.dynamic.sentinel.dto;
+
+/**
+ * @Author 11123357
+ * @Date 2022/11/16 18:16
+ * @Version 1.0
+ */
+public class CommentReason {
+}
---
+++ src/main/java/com/vivo/internet/dynamic/sentinel/util/ExcelUtil.java
@@ -0,0 +1,9 @@
+package com.vivo.internet.dynamic.sentinel.util;
+
+/**
+ * @Author 11123357
+ * @Date 2022/11/16 18:14
+ * @Version 1.0
+ */
+public class ExcelUtil {
+}
